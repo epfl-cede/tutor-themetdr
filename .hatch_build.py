505a91ce7de05db03e904260856b13fa0ffae461
Oleg Demakov <oleg.demakov@epfl.ch>
2025-08-13 10:54:31 +0200

    rename folders, vars, etc

--- .hatch_build.py
+++ .hatch_build.py
@@ -15,6 +15,8 @@ class JSONMetaDataHook(MetadataHookInterface):
 
 def load_about() -> dict[str, str]:
     about: dict[str, str] = {}
-    with open(os.path.join(HERE, "tutorindigo", "__about__.py"), "rt", encoding="utf-8") as f:
+    with open(
+        os.path.join(HERE, "tutorthemetdr", "__about__.py"), "rt", encoding="utf-8"
+    ) as f:
         exec(f.read(), about)  # pylint: disable=exec-used
     return about
